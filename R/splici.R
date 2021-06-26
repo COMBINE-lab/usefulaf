4ccd380abcd3ade608e10a0eb9e44702863b36aa
Dongze He <32473855+DongzeHE@users.noreply.github.com>
2021-06-26 19:03:38 -0400

    adding dedup_seqs flag

--- R/splici.R
+++ R/splici.R
@@ -1,4 +1,4 @@
-make_splici_txome <- function(gtf_path, genome_path, read_length, flank_trim_length = 5, output_dir) {
+make_splici_txome <- function(gtf_path, genome_path, read_length, flank_trim_length = 5, output_dir, dedup_seqs=FALSE) {
   # if you get some error from .get_cds_IDX, please try to rerun the code again
   # read length is the scRNA-seq read length
   # flank trim length is used to avoid marginal case when dealing with junctional reads 
@@ -115,10 +115,11 @@ make_splici_txome <- function(gtf_path, genome_path, read_length, flank_trim_len
     transcripts = grl
   )
   
-  # For future development purpose
-  # seqs = unique(seqs)
-  # grl = grl[names(seqs)]
-  
+  # If having duplicated sequences, only keep one
+  if(dedup_seqs) {
+    seqs = unique(seqs)
+    grl = grl[names(seqs)]
+  }
   
   # save some space
   rm(x)
