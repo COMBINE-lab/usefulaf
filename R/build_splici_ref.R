ad7c931b0a5e0264fcd082b797e3c07371b75ecd
Dongze He <32473855+DongzeHE@users.noreply.github.com>
2021-07-03 22:12:16 -0400

    adding missing periods

--- R/build_splici_ref.R
+++ R/build_splici_ref.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(argparser, quietly=TRUE)
+
 source("./splici.R")    
 
 
@@ -9,9 +10,9 @@ p <- arg_parser("Build a splici reference from a genome and GTF file.")
 
 # Add command line arguments
 # required arguments
-p <- add_argument(p, "genome", help="path to genome")
-p <- add_argument(p, "gtf", help="GTF file with transcript annotations")
-p <- add_argument(p, "read-length", help="read length (determines flank size)",
+p <- add_argument(p, "genome", help="path to genome.")
+p <- add_argument(p, "gtf", help="GTF file with transcript annotations.")
+p <- add_argument(p, "read-length", help="read length (determines flank size).",
 	type="numeric")
 p <- add_argument(p, "splici-dir", 
 	help="output directory where splici reference information will be written.")
@@ -22,11 +23,11 @@ p <- add_argument(p, "--flank-trim-length",
 	type="numeric",
 	default=5)
 p <- add_argument(p, "--extra-spliced",
-	help="path to FASTA file with any extra spliced sequences to include")
+	help="path to FASTA file with any extra spliced sequences to include.")
 p <- add_argument(p, "--extra-unspliced",
-	help="path to FASTA file with any extra unspliced sequences to include")
+	help="path to FASTA file with any extra unspliced sequences to include.")
 p <- add_argument(p, "--dedup-seqs", 
-	help="deduplicate identical sequences before writing to output FASTA", 
+	help="deduplicate identical sequences before writing to output FASTA.", 
 	flag=TRUE)
 
 # Parse the command line arguments
