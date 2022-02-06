344058f4a14e86616e6373b2ac762fd2c2a24613
Rob Patro <rob-p@users.noreply.github.com>
2022-02-06 00:12:25 -0500

    improved load_fry interface to allow loading submatrices into arbitrary layers

--- python/load_fry.py
+++ python/load_fry.py
@@ -1,6 +1,6 @@
 import scanpy
 
-def load_fry(frydir, which_counts=['S','A'], verbose=False):
+def load_fry(frydir, which_counts={'X' : ['S','A']}, verbose=False):
     import json
     import os
     import pandas as pd
@@ -34,8 +34,21 @@ def load_fry(frydir, which_counts=['S','A'], verbose=False):
         ng = int(ng/3)
         if verbose:
             print("processing input in USA mode, will return {}".format("+".join(which_counts)))
+        
+        # make sure the specification in which_counts is OK
+        if 'X' not in which_counts:
+            if verbose:
+                print('In USA mode some sub-matrices must be assigned to the \"X\" (default) output.')
+            return None
+        for k,v in which_counts.items():
+            valid_elem = len(set(v) - set(['U', 'S', 'A'])) == 0
+            if not valid_elem:
+                if verbose:
+                    print(f'Found non-USA element in which_count element list \"{v}\" for key \"{k}\"; cannot proceed.')
+                return None 
+
     elif verbose:
-        print("processing input in standard mode, will return spliced count")
+        print("Processing input in standard mode, will return processed count (which_count will be ignored).")
 
     af_raw = scanpy.read_mtx(os.path.sep.join([frydir, "alevin", "quants_mat.mtx"]))
     afg = [ l.rstrip() for l in open(os.path.sep.join([frydir, "alevin", "quants_mat_cols.txt"])).readlines()][:ng]
@@ -47,14 +60,23 @@ def load_fry(frydir, which_counts=['S','A'], verbose=False):
     abc_df.index = abc_df["barcodes"]
     
     x = af_raw.X
-    if usa_mode:
+    if not usa_mode:
+        af = scanpy.AnnData(x.T, var=abc_df, obs=afg_df)
+        af = af.T 
+    else: # USA mode
         rd = {'S' : range(0,ng), 'U' : range(ng, 2*ng), 'A' : range(2*ng,3*ng)}
-        o = x[:, rd[which_counts[0]]]
-        for wc in which_counts[1:]:
+        xcounts = which_counts['X']
+        o = x[:, rd[xcounts[0]]]
+        for wc in xcounts[1:]:
             o += x[:, rd[wc]]
-    else:
-        o = x
-        
-    af = scanpy.AnnData(o.T, var=abc_df, obs=afg_df)
-    af = af.T
+        af = scanpy.AnnData(o.T, var=abc_df, obs=afg_df)
+        af = af.T
+
+        # now, if there are other layers requested, populate those
+        for other_layer in which_counts.keys() - 'X':
+            xcounts = which_counts[other_layer]
+            o = x[:, rd[xcounts[0]]]
+            for wc in xcounts[1:]:
+                o += x[:, rd[wc]] 
+            af.layers[other_layer] = o
     return af
